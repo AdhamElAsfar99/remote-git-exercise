cffac5892cf468e8e812cb930eea6e64a5ab86be
Adham ElAsfar <adham_samy@aucegypt.edu>
2022-03-15 14:36:01 +0200

    added are you ready message inside SampleCode.cpp

--- CodeSample.cpp
+++ CodeSample.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello, This is a Demo for Git and GitHub \n";
+	cout << "Are you ready? \n";
 	int id;
 	cout << "Enter Your ID: " << endl;
 	cin >> id;
